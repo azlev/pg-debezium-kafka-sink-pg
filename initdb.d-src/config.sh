b3512783d9147415aab85b0784fffa2fae1960d7
Jose Arthur Benetasso Villanova <jose.villanova@loggi.com>
2021-08-31 15:39:43 -0300

    Use only one postgres instance
    Makes the PoC lighter removing a PostgreSQL instance.
    
    Now instead of src and dst database, there is only one: `postgres`, with
    2 databases:
    - postgres (origin)
    - destination (where to sink data)
    
    Also, to this work, it's necessary to change the table name using a
    transformation in sink configuration.

--- initdb.d-src/config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres <<-EOSQL
-    ALTER SYSTEM SET wal_level = logical;
-    ALTER SYSTEM SET log_line_prefix = '%m [%p] %q[user=%u,db=%d,app=%a] ';
-    ALTER SYSTEM SET log_statement = 'all';
-EOSQL
-
--- initdb.d/config.sh
+++ initdb.d/config.sh
@@ -4,5 +4,6 @@ psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres <<-EOSQL
     ALTER SYSTEM SET wal_level = logical;
     ALTER SYSTEM SET log_line_prefix = '%m [%p] %q[user=%u,db=%d,app=%a] ';
     ALTER SYSTEM SET log_statement = 'all';
+    CREATE DATABASE destination;
 EOSQL
 
